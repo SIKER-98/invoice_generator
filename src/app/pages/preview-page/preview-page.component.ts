0d6e53361dffe156f1a372e20c1d8bac98f0383f
SIKER-98 <hubert.sikorski@yahoo.com>
2024-10-05 20:40:19 +0200

    summary view

--- src/app/pages/preview-page/preview-page.component.ts
+++ src/app/pages/preview-page/preview-page.component.ts
@@ -1,10 +1,51 @@
-import {Component} from "@angular/core";
+import {Component, OnDestroy, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
+import {CompanyInfo, Product} from "../../models";
+import {HttpClient} from "@angular/common/http";
+import {delay, retry, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-preview-page',
   templateUrl: './preview-page.component.html',
   styleUrls: ['./preview-page.component.scss']
 })
-export class PreviewPageComponent {
-  title = 'invoice_generator';
+export class PreviewPageComponent implements OnInit, OnDestroy {
+  private subscription = new Subscription();
+
+  company?: CompanyInfo;
+  products?: Product[];
+  total: number = 0;
+
+  constructor(private router: Router,
+              private httpClient: HttpClient) {
+    const navigation = this.router.getCurrentNavigation();
+    this.products = navigation?.extras.state?.['products'] as Product[];
+  }
+
+  ngOnInit(): void {
+    this.getCompanyData();
+    this.calculateTotal();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  private getCompanyData(): void {
+    this.subscription.add(this.httpClient.get('http://localhost:3000/api/company')
+      .pipe(
+        retry(3),
+        delay(1000)
+      )
+      .subscribe({
+        next: data => this.company = data as CompanyInfo,
+        error: err => console.error(err)
+      }));
+  }
+
+  private calculateTotal(): void {
+    if (this.products) {
+      this.total = this.products.reduce((acc: number, product: Product) => acc + product.count * product.price, 0);
+    }
+  }
 }
